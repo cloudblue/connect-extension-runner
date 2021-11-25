72a46fb754cc6743a3bdde9f87cb09ae0ec37737
Francesco Faraone <francesco.faraone@ingrammicro.com>
2021-11-25 09:31:00 +0100

    LITE-21250 partial update of dynamic config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,5 @@
 from connect.eaas.config import ConfigHelper
+from connect.eaas.dataclasses import ConfigurationPayload
 
 
 def test_get_user_agent(mocker):
@@ -12,3 +13,58 @@ def test_get_user_agent(mocker):
     config.env = {'environment_id': '1', 'instance_id': '2'}
     expected_ua = 'connect-extension-runner/22.0 1/3.15 Linux/1.0 1/2'
     assert config.get_user_agent() == {'User-Agent': expected_ua}
+
+
+def test_update_dynamic_config():
+    config = ConfigHelper()
+    payload = ConfigurationPayload(
+        configuration={'var': 'value'},
+        log_level='DEBUG',
+        runner_log_level='INFO',
+        service_id='SRVC-0001',
+        product_id='PRD-000',
+        hub_id='HB-0000',
+        account_id='VA-000',
+        account_name='Acme Inc',
+        logging_api_key='logging_api_key',
+        environment_type='production',
+    )
+
+    config.update_dynamic_config(payload)
+    assert config.variables == payload.configuration
+    assert config.service_id == payload.service_id
+    assert config.product_id == payload.product_id
+    assert config.hub_id == payload.hub_id
+    assert config.account_id == payload.account_id
+    assert config.account_name == payload.account_name
+    assert config.logging_api_key == payload.logging_api_key
+    assert config.environment_type == payload.environment_type
+
+    payload2 = ConfigurationPayload(
+        log_level='WARNING',
+        runner_log_level='ERROR',
+    )
+
+    config.update_dynamic_config(payload2)
+    assert config.variables == payload.configuration
+    assert config.service_id == payload.service_id
+    assert config.product_id == payload.product_id
+    assert config.hub_id == payload.hub_id
+    assert config.account_id == payload.account_id
+    assert config.account_name == payload.account_name
+    assert config.logging_api_key == payload.logging_api_key
+    assert config.environment_type == payload.environment_type
+
+    payload3 = ConfigurationPayload(
+        configuration={'var2': 'value2'},
+    )
+
+    config.update_dynamic_config(payload3)
+    assert config.variables == payload3.configuration
+    assert config.service_id == payload.service_id
+    assert config.product_id == payload.product_id
+    assert config.hub_id == payload.hub_id
+    assert config.account_id == payload.account_id
+    assert config.account_name == payload.account_name
+    assert config.logging_api_key == payload.logging_api_key
+    assert config.environment_type == payload.environment_type
